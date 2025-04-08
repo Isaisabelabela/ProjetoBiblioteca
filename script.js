13ee2466f1445d766c5ed9922057c88c70e7469f
Isabela de Paula Barbosa <83958659+Isaisabelabela@users.noreply.github.com>
2025-04-08 15:58:25 -0300

    Melhoria: validação dos campos do formulário - impedir que o card seja criado se algum campo estiver vazio - mostrar uma mensagem de erro visível para o usuário

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const form = document.getElementById('book-form');
 const container = document.querySelector('.cards-container');
 
+const errorMessage = document.createElement('p');
+errorMessage.style.color = 'red';
+errorMessage.style.textAlign = 'center';
+errorMessage.style.scrollMarginTop = '10px';
+form.appendChild(errorMessage);
+
 form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
+  event.preventDefault(); 
 
   const title = document.getElementById('book-title').value;
   const description = document.getElementById('book-description').value;
   const price = parseFloat(document.getElementById('book-price').value).toFixed(2);
   const imageUrl = document.getElementById('book-image').value;
+
+  if (!title || !description || !price || !imageUrl){
+    errorMessage.textContent = "Por favor, preencha todos os campos corretamente!";
+    return;
+  }
+
+  errorMessage.textContent = "";
 
   const newCard = document.createElement('div');
   newCard.classList.add('card');
@@ -20,6 +33,5 @@ form.addEventListener('submit', function(event) {
 
   container.appendChild(newCard);
 
-  // Limpa os campos depois de adicionar
   form.reset();
 });
